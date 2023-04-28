4d389c2f28f8d0329ffccb06593a692b085e7f40
shen-guohua <haileeshen@icloud.com>
2023-04-28 11:09:33 +0800

    新建代码库

--- demo/scripts/spire/createentry_workload.sh
+++
@@ -1,9 +0,0 @@
-# bin/spire-server entry create \
-#  -parentID spiffe://example.org/myagent \
-#  -spiffeID spiffe://example.org/myservice \
-#  -selector unix:uid:$(id -u)
-
-bin/spire-server entry create \
-   -parentID spiffe://example.org/myagent \
-   -spiffeID spiffe://example.org/myservice \
-   -selector unix:uid:1000
--- demo/scripts/spire/runagent.sh
+++
@@ -1,1 +0,0 @@
-bin/spire-agent run -config conf/agent/agent.conf -joinToken 16047f93-a133-494e-865e-033617d33eec
