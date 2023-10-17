781a3050c5862aed4cf35ae8509dd605e598d9e8
王朝州 <czwang@whu.edu.cn>
2023-10-17 13:14:47 +0000

    vtzproxy
    Signed-off-by: 王朝州 <czwang@whu.edu.cn>

---
+++ trustzone-awared-vm/Host/vtzb_proxy/comm_structs.h
@@ -0,0 +1,297 @@
+#ifndef COMM_STRUCTS_H
+#define COMM_STRUCTS_H
+
+#include <linux/types.h>
+#include "tc_ns_client.h"
+
+#define CERT_BUF_MAX_SIZE        2048
+
+#define TC_NS_CLIENT_DEV_FLAG    3
+#define TC_PRIVATE_DEV_FLAG      4
+#define TC_CVM_DEV_FLAG          5
+#define TLOG_DEV_FLAG            6
+
+#define VTZ_OPEN_TZD            15
+#define VTZ_CLOSE_TZD           17
+#define VTZ_LOG_IN_NHIDL        19
+#define VTZ_GET_TEE_VERSION     21
+#define VTZ_GET_TEE_INFO        23
+#define VTZ_LATE_INIT           25
+#define VTZ_SYNC_TIME           27
+#define VTZ_LOG_IN              29
+#define VTZ_OPEN_SESSION        31
+#define VTZ_SEND_CMD            33
+#define VTZ_CANCEL_CMD          35
+#define VTZ_MMAP                37
+#define VTZ_MUNMAP              39
+#define VTZ_CLOSE_SESSION       41
+#define VTZ_CLOSE_PTZDEV        43
+#define VTZ_FS_REGISTER_AGENT   45
+#define VTZ_WAIT_EVENT			 49
+#define VTZ_SEND_EVENT_RESPONSE 51
+#define VTZ_LOAD_SEC            53
+#define VTZ_TEST                47
+
+#define	VTZ_GET_TEEOS_VER       55
+#define	VTZ_SET_READER_CUR      57 
+#define VTZ_SET_TLOGCAT_STAT    59
+#define VTZ_GET_TLOGCAT_STAT    61
+#define VTZ_GET_LOG             63
+#define VTZ_NOTHING             67
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t seq_num;
+	uint32_t ret;
+} struct_packet_rsp_general;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t cmd;
+	uint32_t seq_num;
+} struct_packet_cmd_test;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t seq_num;
+	uint32_t ret;
+} struct_packet_rsp_test;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t seq_num;
+	uint32_t ret;
+	uint32_t alive;
+} struct_packet_rsp_monitor;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t cmd;
+	uint32_t seq_num;
+	uint32_t vmid;
+	uint32_t flag;
+} struct_packet_cmd_open_tzd;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t seq_num;
+	uint32_t ret;
+	int32_t ptzfd;
+} struct_packet_rsp_open_tzd;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t cmd;
+	uint32_t seq_num;
+	int32_t ptzfd;
+} struct_packet_cmd_close_tzd;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t seq_num;
+	uint32_t ret;
+} struct_packet_rsp_close_tzd;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t cmd;
+	uint32_t seq_num;
+	int32_t ptzfd;
+} struct_packet_cmd_getteever;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t seq_num;
+	uint32_t ret;
+	uint32_t tee_ver;
+} struct_packet_rsp_getteever;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t cmd;
+	uint32_t seq_num;
+	int32_t ptzfd;
+	bool istlog;
+} struct_packet_cmd_getteeinfo;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t seq_num;
+	uint32_t ret;
+    TC_NS_TEE_Info info;
+} struct_packet_rsp_getteeinfo;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t cmd;
+	uint32_t seq_num;
+	int32_t ptzfd;
+	void  *vmaddr;
+	struct AgentIoctlArgs args;
+} struct_packet_cmd_regagent;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t seq_num;
+	uint32_t ret;
+	struct AgentIoctlArgs args;
+} struct_packet_rsp_regagent;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t cmd;
+	uint32_t seq_num;
+	int32_t ptzfd;
+	uint32_t agent_id;
+} struct_packet_cmd_event;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t cmd;
+	uint32_t seq_num;
+	uint32_t index;
+} struct_packet_cmd_lateinit;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t seq_num;
+	uint32_t ret;
+} struct_packet_rsp_lateinit;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t cmd;
+	uint32_t seq_num;
+	int32_t ptzfd;	
+	TC_NS_Time tcNsTime;
+} struct_packet_cmd_synctime;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t seq_num;
+	uint32_t ret;
+} struct_packet_rsp_synctime;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t cmd;
+	uint32_t seq_num;
+	int32_t ptzfd;
+	uint8_t cert_buffer[CERT_BUF_MAX_SIZE];
+} struct_packet_cmd_login;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t cmd;
+	uint32_t seq_num;
+	int32_t ptzfd;
+} struct_packet_cmd_login_non;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t seq_num;
+	uint32_t ret;
+} struct_packet_rsp_login;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t cmd;
+	uint32_t seq_num;
+	int32_t ptzfd;
+	__s32 cpu_index;
+	struct SecLoadIoctlStruct ioctlArg;
+} struct_packet_cmd_load_sec;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t seq_num;
+	uint32_t ret;
+	struct SecLoadIoctlStruct ioctlArg;
+} struct_packet_rsp_load_sec;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t cmd;
+	uint32_t seq_num;
+	int32_t ptzfd;
+	__s32 cpu_index;
+	TC_NS_ClientContext cliContext;
+} struct_packet_cmd_session;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t seq_num;
+	uint32_t ret;
+	TC_NS_ClientContext cliContext;
+} struct_packet_rsp_session;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t cmd;
+	uint32_t seq_num;
+	int32_t ptzfd;
+	int32_t err_flag;
+	uint32_t vm_page_size;
+	uint64_t block_addrs[4];//qemu and proxy don't use
+	uint32_t block_size[4];
+	unsigned long long addrs[4]; //used by ref mem mmap
+	TC_NS_ClientContext cliContext;
+} struct_packet_cmd_send_cmd;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t seq_num;
+	uint32_t ret;
+	TC_NS_ClientContext cliContext;
+} struct_packet_rsp_send_cmd;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t cmd;
+	uint32_t seq_num;
+	int32_t ptzfd;
+	__s32 cpu_index;
+	TC_NS_ClientContext cliContext;
+	pid_t pid;
+} struct_packet_cmd_cancel_cmd;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t seq_num;
+	uint32_t ret;
+	TC_NS_ClientContext cliContext;
+} struct_packet_rsp_cancel_cmd;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t cmd;
+	uint32_t seq_num;
+	int32_t ptzfd;
+	uint64_t buffer;
+	uint32_t size;
+	uint32_t offset;
+} struct_packet_cmd_mmap;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t seq_num;
+	uint32_t ret;
+} struct_packet_rsp_mmap;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t cmd;
+	uint32_t seq_num;
+} struct_packet_cmd_nothing;
+
+typedef struct {
+	uint32_t packet_size;
+	uint32_t seq_num;
+	uint32_t ret;
+} struct_packet_rsp_nothing;
+
+#endif
+
+
+
+
---
+++ trustzone-awared-vm/Host/vtzb_proxy/tlogcat.c
@@ -0,0 +1,161 @@
+#include "tlogcat.h"
+#include <errno.h>
+#include <error.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "securec.h"
+#include "debug.h"
+#include "serial_port.h"
+#include "comm_structs.h"
+
+static void *g_log_buffer = NULL;
+static char g_log_teeVersion[MAX_TEE_VERSION_LEN];
+
+static void tlog_get_teever(struct_packet_cmd_get_ver *packet_cmd,
+    struct serial_port_file *serial_port)
+{
+    int ret = 0;
+    struct_packet_rsp_get_ver packet_rsp;
+    packet_rsp.seq_num = packet_cmd->seq_num + 1;
+    packet_rsp.packet_size = sizeof(packet_rsp);
+    debug("*****cmd is tlog_get_teever*****\n");
+    ret = ioctl(packet_cmd->ptzfd, TEELOGGER_GET_VERSION, g_log_teeVersion);
+    packet_rsp.ret = ret;
+    debug("   ret = %d\n", ret);
+    if (memcpy_s(packet_rsp.version_info, MAX_TEE_VERSION_LEN,
+        g_log_teeVersion, MAX_TEE_VERSION_LEN)) {
+        tloge("memcpy_s err \n");
+        debug("memcpy_s err\n");
+    }
+    ret = send_to_vm(serial_port, &packet_rsp, sizeof(packet_rsp));
+    if (ret != sizeof(packet_rsp)) {
+        tloge("send to VM failed \n");
+    }
+}
+
+static void tlog_set_reader_cur(
+    struct_packet_cmd_set_reader_cur *packet_cmd,
+    struct serial_port_file *serial_port)
+{
+    int ret = 0;
+    struct_packet_rsp_set_reader_cur packet_rsp;
+
+    debug("*****cmd is tlog_set_reader_cur*****\n");
+    packet_rsp.seq_num = packet_cmd->seq_num + 1;
+    packet_rsp.packet_size = sizeof(packet_rsp);
+    ret = ioctl(packet_cmd->ptzfd, TEELOGGER_SET_READERPOS_CUR, 0);
+    packet_rsp.ret = ret;
+    ret = send_to_vm(serial_port, &packet_rsp, sizeof(packet_rsp));
+    if (ret != sizeof(packet_rsp)) {
+        tloge("send to VM failed \n");
+    }
+}
+
+static void tlog_set_stat(struct_packet_cmd_set_tlogcat_stat *packet_cmd,
+    struct serial_port_file *serial_port)
+{
+    int ret = 0;
+    struct_packet_rsp_set_tlogcat_stat packet_rsp;
+    debug("*****cmd is tlog_set_stat*****\n");
+    packet_rsp.seq_num = packet_cmd->seq_num + 1;
+    packet_rsp.packet_size = sizeof(packet_rsp);
+    ret = ioctl(packet_cmd->ptzfd, TEELOGGER_SET_TLOGCAT_STAT, 0);
+    packet_rsp.ret = ret;
+    ret = send_to_vm(serial_port, &packet_rsp, sizeof(packet_rsp));
+    if (ret != sizeof(packet_rsp)) {
+        tloge("send to VM failed \n");
+    }
+}
+
+static void tlog_get_stat(struct_packet_cmd_get_tlogcat_stat *packet_cmd,
+    struct serial_port_file *serial_port)
+{
+    int ret = 0;
+    struct_packet_rsp_get_tlogcat_stat packet_rsp;
+    debug("*****cmd is tlog_get_stat*****\n");
+    packet_rsp.seq_num = packet_cmd->seq_num + 1;
+    packet_rsp.packet_size = sizeof(packet_rsp);
+    ret = ioctl(packet_cmd->ptzfd, TEELOGGER_GET_TLOGCAT_STAT, 0);
+    packet_rsp.ret = ret;
+    ret = send_to_vm(serial_port, &packet_rsp, sizeof(packet_rsp));
+    if (ret != sizeof(packet_rsp)) {
+        tloge("send to VM failed \n");
+    }
+}
+
+static void tlog_get_log(struct_packet_cmd_get_log *packet_cmd,
+    struct serial_port_file *serial_port)
+{
+    int32_t result;
+    int32_t ret = 0;
+    fd_set readset;
+    struct_packet_rsp_get_log packet_rsp;
+    debug("*****cmd is GET LOG*****\n");
+    packet_rsp.seq_num = packet_cmd->seq_num + 1;
+    packet_rsp.packet_size = sizeof(packet_rsp);
+    while(1){
+        do {
+            FD_ZERO(&readset);
+            FD_SET(packet_cmd->ptzfd, &readset);
+            tlogd("while select\n");
+            result = select((packet_cmd->ptzfd + 1), &readset, NULL, NULL, NULL);
+        } while (result == -1 && errno == EINTR);
+        if (result < 0) {
+            continue;
+        }
+        ret = read(packet_cmd->ptzfd, g_log_buffer, LOG_BUFFER_LEN);
+        debug("log length = %d \n", ret);
+        packet_rsp.length = ret;
+        dump_buff(g_log_buffer, 32);
+        if(memcpy_s(packet_rsp.buffer, sizeof(packet_rsp.buffer), g_log_buffer, ret)) {
+            tloge("memcpy_s err\n");
+            debug("memcpy_s err\n");
+            break;
+        }
+        //packet_rsp.ret = ret;
+        //dump_buff(packet_rsp.buffer, 32);
+        ret = send_to_vm(serial_port, &packet_rsp, sizeof(packet_rsp));
+        if (ret != sizeof(packet_rsp)) {
+            tloge("send to VM failed \n");
+        }
+        break;
+    }
+}
+
+void tlog(uint32_t cmd, void *packet_cmd,struct serial_port_file *serial_port)
+{
+    switch (cmd)
+    {
+    case VTZ_GET_TEEOS_VER:
+        (void)tlog_get_teever((struct_packet_cmd_get_ver *)packet_cmd,
+            serial_port);
+        break;
+    case VTZ_SET_READER_CUR:
+        (void)tlog_set_reader_cur((struct_packet_cmd_set_reader_cur *)packet_cmd,
+            serial_port);
+        break;
+    case VTZ_SET_TLOGCAT_STAT:
+        (void)tlog_set_stat((struct_packet_cmd_set_tlogcat_stat *)packet_cmd,
+            serial_port);
+        break;
+    case VTZ_GET_TLOGCAT_STAT:
+        (void)tlog_get_stat((struct_packet_cmd_get_tlogcat_stat *)packet_cmd,
+            serial_port);
+        break;
+    case VTZ_GET_LOG:
+        (void)tlog_get_log((struct_packet_cmd_get_log *)packet_cmd,
+            serial_port);
+    default:
+        break;
+    }
+}
+
+int init_tlog()
+{
+    g_log_buffer = malloc(LOG_BUFFER_LEN);
+    if (!g_log_buffer) {
+        tloge("Failed to allocate memory\n");
+        return -ENOMEM;       
+    }
+    return 0;
+}
