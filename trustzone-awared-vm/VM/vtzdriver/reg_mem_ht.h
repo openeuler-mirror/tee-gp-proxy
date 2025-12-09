7437ae7f9da8ea43400af400abe0097d63869ae8
gongchangsui <gongchangsui@outlook.com>
2025-12-09 02:54:04 -0500

    register memory should be alloc in session open and release in session close
    Signed-off-by: gongchangsui <gongchangsui@outlook.com>

---
+++ trustzone-awared-vm/VM/vtzdriver/reg_mem_ht.h
@@ -0,0 +1,15 @@
+#include <linux/kernel.h>
+#include <linux/hashtable.h>
+#include "teek_ns_client.h"
+#define HT_BITS 10
+struct reg_mem {
+    uint32_t session_id;
+    uint64_t block_addrs[TEE_PARAM_NUM];
+    uint64_t block_size[TEE_PARAM_NUM];
+    uintptr_t addrs[TEE_PARAM_NUM][3];
+    uint32_t page_size;
+    struct hlist_node node;
+};
+int add_reg_mem(struct reg_mem * p_reg);
+struct reg_mem * find_reg_mem(uint32_t session_id);
+int del_reg_mem(uint32_t session_id);
